f6e4ef7fe267062a9332732b90aabac26c8d5bd9
cyberaa <jjesusdei90@gmail.com>
2015-10-26 10:14:25 +0000

    **Added JQuery
    **Added
    - JQuery

---
+++ JQUERY/add_remove_Class.js
@@ -0,0 +1,6 @@
+/* ADD and Remove Class function */
+
+/* button gets everything that is on the class test */
+$("button").addClass("test");
+/* the element with the id button-properties loses the class button-default */
+$("#button-properties").removeClass("button-default");
---
+++ JQUERY/ready.js
@@ -0,0 +1,10 @@
+/* ready waits for everything on the page to be rendered before it runs everything inside the function */
+$(document).ready(function(){
+
+}
+                  
+/* Examples */
+/* Adds a class to .class */
+$(document).ready(function(){
+    $(".class").addClass("");
+}
